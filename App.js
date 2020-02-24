41dc579cfa94a4d6ddabe60072fb496848c0fda4
swetah3 <35355633+swetah3@users.noreply.github.com>
2020-02-24 15:03:37 +0530

    login data

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+ import Login from './Home/Login';
+ import Signup from './Home/signup';
+
+function App() {
+  return (
+    <div className="">
+     <Signup/>  */}
+     {/* <Login  images="images/login.png"/>  */}
+    </div>
+  );
+}
+
+export default App;
